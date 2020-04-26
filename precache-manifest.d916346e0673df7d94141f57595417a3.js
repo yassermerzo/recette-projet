de1d892cf10dce04d49de6b38c568420b84baf9c
yassermerzo <yassermerzouki@gmail.com>
2020-04-26 20:47:19 +0000

    Updates

---
+++ precache-manifest.d916346e0673df7d94141f57595417a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1b404c1c5f5ae6589a5ad440933b04",
+    "url": "/recette-projet/index.html"
+  },
+  {
+    "revision": "2a7af9573c879a469da7",
+    "url": "/recette-projet/static/css/main.6ff40f4b.chunk.css"
+  },
+  {
+    "revision": "cb7da185838abb13a8d4",
+    "url": "/recette-projet/static/js/2.f5166725.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/recette-projet/static/js/2.f5166725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7af9573c879a469da7",
+    "url": "/recette-projet/static/js/main.a5011734.chunk.js"
+  },
+  {
+    "revision": "86fd365d1c2e39efba0c",
+    "url": "/recette-projet/static/js/runtime-main.5b18d0ff.js"
+  }
+]);
